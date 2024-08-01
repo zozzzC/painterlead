16aef0b63eca4e910cee479d8d1aa7607c6db7b0
zozzzC <czlc05edu@gmail.com>
2024-08-01 16:19:52 +1200

    added plus button component

---
+++ painterlead/src/components/general/RoundButton.tsx
@@ -0,0 +1,11 @@
+import { PlusSignCircleIcon } from "hugeicons-react";
+
+export default function RoundButton() {
+  return (
+    <div className="absolute bottom-0 right-0 p-10">
+      <button>
+        <PlusSignCircleIcon size={50} />
+      </button>
+    </div>
+  );
+}
