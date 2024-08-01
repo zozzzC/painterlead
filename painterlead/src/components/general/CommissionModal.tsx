63a834193220911c2d60a8fdb42eb238706d4436
zozzzC <czlc05edu@gmail.com>
2024-08-01 16:41:39 +1200

    added TagsSection and CommissionModal basic start

---
+++ painterlead/src/components/general/CommissionModal.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function CommissionModal() { 
+    return ( 
+        <div className="absolute p-10 outline-4 outline-lightest-grey w-full h-full">
+            
+        </div>
+    );
+}
