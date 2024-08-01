1b8142defb9c48cfcec1ec42e10c07ca529b6d7b
zozzzC <czlc05edu@gmail.com>
2024-08-01 20:48:39 +1200

    change to make return of errors the same as zod error response

--- backend/src/controllers/signin/login.ts
+++ backend/src/controllers/signin/login.ts
@@ -1,40 +1,47 @@
-import bcrypt from "bcrypt";
-import { PrismaClient } from "@prisma/client";
-import { DefaultArgs } from "@prisma/client/runtime/library";
-import { Jwt } from "jsonwebtoken";
+import bcrypt from 'bcrypt';
+import { PrismaClient } from '@prisma/client';
+import { DefaultArgs } from '@prisma/client/runtime/library';
+import { Jwt } from 'jsonwebtoken';
+import { string } from 'zod';
 const prisma = new PrismaClient();
 
-export default async function loginUser({email, password} : {email: string; password: string;}) { 
-    try { 
-        const errors: {[key: string]: any} = {};
-        errors["error"] = {};  
-    
+export default async function loginUser({
+    email,
+    password,
+}: {
+    email: string;
+    password: string;
+}) {
+    try {
+        const errors: { [key: string]: any } = {};
+        errors['error'] = {};
+
         const findEmail = await prisma.user.findUnique({
-            where: { 
-                email: email
+            where: {
+                email: email,
+            },
+        });
+
+        if (findEmail === null) {
+            errors.error.email = [];
+            errors.error.email[0] = 'Email has not been registered.';
+            if (JSON.stringify(errors.error) !== null) {
+                return errors;
             }
-        })
-        
-        if (findEmail === null) { 
-            errors.error.email = "Email has not been registered."
-            if (JSON.stringify(errors.error) !== null) { 
-                return errors.error;
-            }
-        }  else { 
+        } else {
             //NOTE: this has to be an await since if we used the default
-            //bcrypt compare implementation, then it would not wait for it to execute and would 
+            //bcrypt compare implementation, then it would not wait for it to execute and would
             //execute other code before it since bcrypt.compare was async and did not return a promise
             const match = await bcrypt.compare(password, findEmail.password);
-            if (!match) { 
-                errors.error.password = "Passwords do not match."
-                return errors.error;
-            } 
+            if (!match) {
+                errors.error.password = [];
+                errors.error.password[0] = 'Passwords do not match.';
+                return errors;
+            }
         }
 
         return {};
-
-    } catch (err) { 
-        return err;
+    } catch (err: any) {
+        throw new Error(err.message);
     }
-
 }
