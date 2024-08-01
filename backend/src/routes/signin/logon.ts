096f0552ff659c499fdb1bc72607d80cbcead6d5
zozzzC <czlc05edu@gmail.com>
2024-08-01 20:48:39 +1200

    change to make return of errors the same as zod error response

--- backend/src/routes/signin/logon.ts
+++ backend/src/routes/signin/logon.ts
@@ -1,25 +1,29 @@
-import express from "express";
-import { LoginUserSchema } from "../../schema/user";
-import validateReq from "../../helpers/zodValidationGeneric";
-import loginUser from "../../controllers/signin/login";
+import express from 'express';
+import { LoginUserSchema } from '../../schema/user';
+import validateReq from '../../helpers/zodValidationGeneric';
+import loginUser from '../../controllers/signin/login';
 const router = express.Router();
 
-router.post("/", validateReq(LoginUserSchema), async (req: express.Request, res: express.Response) => { 
-    try { 
-        const { email, password  } = req.body;
-        const user = await loginUser({ email, password });
+interface User {
+        [key: string]: any;
+}
 
-        console.log(user)
-
-
-        if (Object.keys(user).length === 0) { 
-            res.status(201).json(req.body);
-        } else { 
-            res.status(400).json(user);
+router.post(
+    '/',
+    validateReq(LoginUserSchema),
+    async (req: express.Request, res: express.Response) => {
+        try {
+            const { email, password } = req.body;
+            const user = await loginUser({ email, password });
+            if (Object.keys(user).length == 0) {
+                res.status(201).json(req.body);
+            } else {
+                res.status(400).json(user);
+            }
+        } catch (err) {
+            res.status(500).send(err);
         }
-    } catch (err) { 
-        res.status(500).send(err);
-    }
-})
+    },
+);
 
 export default router;
