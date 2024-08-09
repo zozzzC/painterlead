d59c138cfb558cfd6e24704bf3696045d6ee43fa
zozzzC <czlc05edu@gmail.com>
2024-08-09 17:33:54 +1200

    fixed spacing between commission cards

--- painterlead/src/components/general/CommissionModal.tsx
+++ painterlead/src/components/general/CommissionModal.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function CommissionModal({ id }: { id: Number }) {
   return (
-    <div className="absolute p-10 outline-4 outline-lightest-grey w-full h-full">
+    <div className="outline-4 bg-red outline-lightest-grey w-full h-full">
       <p>hiya</p>
     </div>
   );
