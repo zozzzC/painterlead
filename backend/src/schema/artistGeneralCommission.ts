3740ff6cc4ebcff94d321187b61f04add38e9665
zozzzC <czlc05edu@gmail.com>
2024-08-11 15:39:54 +1200

    add edit commission routes

--- backend/src/schema/artistGeneralCommission.ts
+++ backend/src/schema/artistGeneralCommission.ts
@@ -6,8 +6,11 @@ export const ArtistGeneralCommissionSchema = z.object({
     name: z.string({
         required_error: "Commission name is required",
     }),
-    artistId: z.number(),
+    artistId: z.number({
+        required_error: "ArtistID must be specified",
+    }),
     blurb: z.string().max(100).optional(),
     description: z.string().max(500),
+    
     // status: z.enum(Status)
 })
