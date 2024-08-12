4d9858ad2d207dc8db40abc8948d733789e8f83f
zozzzC <czlc05edu@gmail.com>
2024-08-13 11:49:17 +1200

    added more logic to editCommission post request create route

--- backend/src/routes/edit/editCommission.ts
+++ backend/src/routes/edit/editCommission.ts
@@ -7,29 +7,112 @@ const prisma = new PrismaClient();
 const router = express.Router();
 
 router.get(
-    '/:artistId',
-    validateReq(ArtistGeneralCommissionSchema),
-    verifyJWT(),
-    async (req: Express.Request, res: Express.Response) => {
+    '/artist/:artistId',
+    async (req: express.Request, res: express.Response) => {
         try {
             const artistId = req?.params['artistId'];
 
             if (artistId) {
-                const findArtistCommissions = await
-                    prisma.artistGeneralCommission.findMany({
+                const findArtistCommissions =
+                    await prisma.artistGeneralCommission.findMany({
                         where: {
-                            artistId: artistId,
+                            artistId: parseInt(artistId),
                         },
                     });
 
-                return findArtistCommissions;
+                return res.status(200).json(findArtistCommissions);
             }
-
         } catch (err) {
             return err;
+            return res.sendStatus(400);
         }
     },
 );
 
-//non-param post is 
-router.post('/')
+router.get('/', async (req: express.Request, res: express.Response) => {
+    console.log('edit get');
+    res.sendStatus(201);
+});
+
+//non-param post means new post
+router.post(
+    '/',
+    validateReq(ArtistGeneralCommissionSchema),
+    verifyJWT(),
+    async (req: express.Request, res: express.Response) => {
+        try {
+            // @ts-ignore
+            const token = req?.token;
+            const commissionId = req?.params['commissionId'];
+            const { name }: { name: string } = req.body;
+
+            const userId = await prisma.user.findUnique({
+                where: {
+                    username: token.username,
+                },
+            });
+
+            if (name && userId) {
+                const nameExists =
+                    await prisma.artistGeneralCommission.findUnique({
+                        //@ts-ignore
+                        where: {
+                            artistId: userId.id,
+                            name: name,
+                        },
+                    });
+
+                if (nameExists) {
+                    res.sendStatus(400);
+                }
+
+                const newCommission =
+                    await prisma.artistGeneralCommission.create({
+                        data: {
+                            ...req.body,
+                        },
+                    });
+
+                res.sendStatus(201);
+            }
+
+            res.sendStatus(400);
+        } catch (err) {
+            res.sendStatus(500);
+        }
+    },
+);
+
+//paramed post means edit existing post
+router.post(
+    '/commission/:commissionId',
+    validateReq(ArtistGeneralCommissionSchema),
+    verifyJWT(),
+    async (req: express.Request, res: express.Response) => {
+        // @ts-ignore
+        const token = req?.token;
+        const commissionId = req?.params['commissionId'];
+        // @ts-ignore
+        if (req.token) {
+            const matchJWTWithCommission =
+                await prisma.artistGeneralCommission.findUnique({
+                    where: {
+                        // @ts-ignore
+                        artistId: parseInt(req.token.username),
+                        id: parseInt(commissionId),
+                    },
+                });
+            // if (matchJWTWithCommission) {
+            //     const updateCommission =
+            //         await prisma.artistGeneralCommission.update({
+            //             where: {
+            //                 id: parseInt(commissionId),
+            //             },
+            //             update: req.body,
+            //         });
+            // }
+        }
+    },
+);
+
+export default router;
